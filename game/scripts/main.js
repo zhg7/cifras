35e9cc5df5bb1ddddce9dc49afd66b46b2a37bd0
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-10 22:06:02 +0100

    soundtrack, modals (broken for now)

--- game/scripts/main.js
+++ game/scripts/main.js
@@ -1,14 +1,32 @@
-document.getElementById('btn-modal').addEventListener('click', function() {
-    document.getElementById('overlay').classList.add('is-visible');
-    document.getElementById('modal').classList.add('is-visible');
-  });
-  
-  document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('overlay').classList.remove('is-visible');
-    document.getElementById('modal').classList.remove('is-visible');
-  });
-  document.getElementById('overlay').addEventListener('click', function() {
-    document.getElementById('overlay').classList.remove('is-visible');
-    document.getElementById('modal').classList.remove('is-visible');
-  });
-  
+/*document.getElementById('help-modal-btn').addEventListener('click', function () {
+  document.getElementById('overlay').classList.add('is-visible');
+  document.getElementById('help-modal').classList.add('is-visible');
+});
+
+document.getElementById('close-help-btn').addEventListener('click', () => {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('help-modal').classList.remove('is-visible');
+});
+
+
+document.getElementById('overlay').addEventListener('click', () => {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('help-modal').classList.remove('is-visible');
+});
+
+document.getElementById('ranking-modal-btn').addEventListener('click', function () {
+  document.getElementById('overlay').classList.add('is-visible');
+  document.getElementById('ranking-modal').classList.add('is-visible');
+})
+
+
+document.getElementById('overlay').addEventListener('click', function () {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('ranking-modal').classList.remove('is-visible');
+});*/
+
+
+// Música de fondo
+const musicPlayer = document.getElementById("soundtrack");
+musicPlayer.play();
+musicPlayer.volume = 0.10;
