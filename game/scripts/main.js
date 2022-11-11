371861e6954b4f2c9b582b98bd1e2db98187b486
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-11 12:52:45 +0100

    modal fix + gitignore

--- game/scripts/main.js
+++ game/scripts/main.js
@@ -1,32 +1,20 @@
-/*document.getElementById('help-modal-btn').addEventListener('click', function () {
-  document.getElementById('overlay').classList.add('is-visible');
-  document.getElementById('help-modal').classList.add('is-visible');
+// Modal de ayuda
+document.getElementById("help-modal-btn").addEventListener("click", () => {
+  document.getElementById("overlay").classList.add("is-visible");
+  document.getElementById("help-modal").classList.add("is-visible");
 });
 
-document.getElementById('close-help-btn').addEventListener('click', () => {
-  document.getElementById('overlay').classList.remove('is-visible');
-  document.getElementById('help-modal').classList.remove('is-visible');
+document.getElementById("overlay").addEventListener("click", () => {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("help-modal").classList.remove("is-visible");
 });
 
-
-document.getElementById('overlay').addEventListener('click', () => {
-  document.getElementById('overlay').classList.remove('is-visible');
-  document.getElementById('help-modal').classList.remove('is-visible');
+document.getElementById("help-close-btn").addEventListener("click", () => {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("help-modal").classList.remove("is-visible");
 });
-
-document.getElementById('ranking-modal-btn').addEventListener('click', function () {
-  document.getElementById('overlay').classList.add('is-visible');
-  document.getElementById('ranking-modal').classList.add('is-visible');
-})
-
-
-document.getElementById('overlay').addEventListener('click', function () {
-  document.getElementById('overlay').classList.remove('is-visible');
-  document.getElementById('ranking-modal').classList.remove('is-visible');
-});*/
-
 
 // Música de fondo
 const musicPlayer = document.getElementById("soundtrack");
 musicPlayer.play();
-musicPlayer.volume = 0.10;
+musicPlayer.volume = 0.1;
