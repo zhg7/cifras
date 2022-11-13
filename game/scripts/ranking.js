f5cd519757251aabf785fad07186651161ee964b
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-13 21:20:09 +0100

    test localstorage, ranking

--- game/scripts/ranking.js
+++ game/scripts/ranking.js
@@ -1,3 +1,24 @@
 document.getElementById("back-btn").addEventListener("click", () => {
     window.location.href = "../../index.html";
 })
+
+const jugador1 = {
+    nombre: "zheng",
+    puntuacion: 30
+}
+
+const jugador2 = {
+    nombre: "alex",
+    puntuacion: 20
+}
+
+const jugadores = [jugador1, jugador2]
+
+localStorage.setItem("jugadores", JSON.stringify(jugadores));
+
+const players = JSON.parse(localStorage.getItem("jugadores"));
+
+players.forEach(player => {
+    console.log(player["nombre"]);
+    console.log(player["puntuacion"]);
+})
