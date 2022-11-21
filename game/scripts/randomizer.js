ea63874ff350bacf922b48e8fe61d1c12493b016
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-21 13:23:44 +0100

    randomizer fixes

--- game/scripts/randomizer.js
+++ game/scripts/randomizer.js
@@ -6,20 +6,36 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100].sort(
   (a, b) => 0.5 - Math.random()
 );
 
+sessionStorage.setItem("generationCounter", 0);
+
 document.querySelector(".flipbox-front").addEventListener("click", () => {
-  const number = numbers[Math.floor(Math.random() * numbers.length)];
-  numbers = numbers.filter((n) => n !== number);
-  document.getElementById("generated-number").textContent = number;
-  const numberSpans = document.querySelectorAll(".number");
-  let inserted = false;
-  for (let i = 0; !inserted; i++) {
-    if (numberSpans[i].textContent === "") {
-      numberSpans[i].textContent = number;
-      inserted = true;
+  if (sessionStorage.getItem("generationCounter") < 6) {
+    sessionStorage.setItem(
+      "generationCounter",
+      Number(sessionStorage.getItem("generationCounter")) + 1
+    );
+
+    const number = numbers[Math.floor(Math.random() * numbers.length)];
+    numbers = numbers.filter((n) => n !== number);
+    document.getElementById("generated-number").textContent = number;
+    const numberSpans = document.querySelectorAll(".number");
+    let inserted = false;
+    for (let i = 0; !inserted; i++) {
+      if (numberSpans[i].textContent === "") {
+        numberSpans[i].textContent = number;
+        inserted = true;
+      }
     }
+    document.querySelector(".flipbox-inner").classList.add("flip");
+    setTimeout(() => {
+      document.querySelector(".flipbox-inner").classList.remove("flip");
+    }, 800);
   }
-  document.querySelector(".flipbox-inner").classList.add("flip");
   setTimeout(() => {
-    document.querySelector(".flipbox-inner").classList.remove("flip");
-  }, 1000);
+    if (sessionStorage.getItem("generationCounter") > 5) {
+      const box = document.querySelector(".flipbox-front");
+      box.style.cursor = "default";
+      box.textContent = "Todos los números generados";
+    }
+  }, 200);
 });
