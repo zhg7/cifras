a73cf20c290632fbe6b61dee94f3f3354d6d5ba2
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-23 13:39:38 +0100

    initial drag and drop code

--- game/scripts/play.js
+++ game/scripts/play.js
@@ -6,4 +6,48 @@
   e.preventDefault();
 })*/
 
-document.getElementById("target-number").textContent = sessionStorage.getItem("targetNumber");
+let availableNumbers = JSON.parse(sessionStorage.getItem("availableNumbers"));
+const numberSpans = document.querySelectorAll(".number span");
+let dragItem = null;
+
+for (let i = 0; i < 6; i++) {
+  numberSpans[i].textContent = availableNumbers[i];
+}
+
+document.getElementById("target-number").textContent =
+  sessionStorage.getItem("targetNumber");
+
+const draggables = document.querySelectorAll("[draggable='true']");
+
+draggables.forEach((element) => {
+  element.addEventListener("dragstart", dragStart);
+  element.addEventListener("dragend", dragEnd);
+});
+
+const droppables = document.querySelectorAll("[droppable='true']");
+
+droppables.forEach((element) => {
+  element.addEventListener("dragover", dragOver);
+  element.addEventListener("dragleave", dragLeave);
+  element.addEventListener("drop", drop);
+});
+
+function dragStart(e) {
+  dragItem = e.target;
+}
+
+function dragEnd(e) {
+  e.preventDefault();
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragLeave(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.target.append(dragItem);
+}
