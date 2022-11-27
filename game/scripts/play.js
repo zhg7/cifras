5ac8435ec29aa5fa1cacf19d1a8a2a000726a297
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-11-27 17:46:53 +0100

    skip turn system

--- game/scripts/play.js
+++ game/scripts/play.js
@@ -5,6 +5,8 @@ if (sessionStorage.getItem("started") !== "yes") {
 
 window.addEventListener("beforeunload", preventAccidentalClose);
 
+document.getElementById("skip-turn").addEventListener("click", changeTurn);
+
 function preventAccidentalClose(e) {
   e.preventDefault();
 }
