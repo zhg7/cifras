afd0a2faa06467cb0ab41cd540ee50b2c9b0e6af
zhg7 <92540253+zhg7@users.noreply.github.com>
2022-12-04 19:46:09 +0100

    operation modal

--- game/scripts/gameover.js
+++ game/scripts/gameover.js
@@ -1,3 +1,16 @@
+document.getElementById("overlay").addEventListener("click", () => {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("operation-log-modal").classList.remove("is-visible");
+  emptyOperationLog()
+});
+
+document.getElementById("operation-log-close-btn").addEventListener("click", () => {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("operation-log-modal").classList.remove("is-visible");
+  emptyOperationLog()
+});
+
+const operationList = document.getElementById("operation-log")
 const playerIcons = document.querySelectorAll(".game-over-player-icon");
 const playerNames = document.querySelectorAll(".game-over-player-name");
 const players = JSON.parse(sessionStorage.getItem("currentPlayers"));
@@ -17,3 +30,32 @@ for (let i = 0; i <= 1; i++) {
       operationLogs[i].insertAdjacentHTML("beforeend", `<li>${operations[i][j]}</li>`);
     }
   }*/
+
+const operationLogButtons = document.querySelectorAll(".view-operations");
+operationLogButtons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    showOperationLog(e.target.parentElement.id)
+  })
+})
+
+function setResults(){
+  
+}
+
+function showOperationLog(player) {
+  const playerName = players[Number(player.substr(-1)) - 1]["name"];
+  const playerOperations = JSON.parse(sessionStorage.getItem(`player${Number(player.substr(-1)) - 1}_operations`));
+  playerOperations.forEach(op => {
+    operationList.insertAdjacentHTML("beforeend", `<li>${op}</li>`);
+  })
+
+  document.getElementById("overlay").classList.add("is-visible");
+  document.getElementById("operation-log-modal").classList.add("is-visible");
+  document.getElementById("player-log").textContent = playerName;
+}
+
+function emptyOperationLog() {
+  while (operationList.firstElementChild) {
+    operationList.removeChild(operationList.firstElementChild)
+  }
+}
